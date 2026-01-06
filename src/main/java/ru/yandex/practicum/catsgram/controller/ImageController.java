ce52279dcfdf24263c4aaa09980c4b2f39dbd6b3
saleevakate <saleevakate@yandex.ru>
2026-01-06 17:09:32 +0500

    коммит 06.01.2026

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.service.ImageService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+    private final ImageService imageService;
+
+    @GetMapping("/posts/{postId}/images")
+    public List<Image> getPostImages(@PathVariable("postId") long postId) {
+        return imageService.getPostImages(postId);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.catsgram.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final Map<Long, Image> images = new HashMap<>();
+
+    // получение данных об изображениях указанного поста
+    public List<Image> getPostImages(long postId) {
+        return images.values()
+                .stream()
+                .filter(image -> image.getPostId() == postId)
+                .collect(Collectors.toList());
+    }
+}
